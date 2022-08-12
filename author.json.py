87f9d595fe2b53a45bbfe63e3cc3cf114f1e6297
IbrahimDellalbasi <111121362+IbrahimDellalbasi@users.noreply.github.com>
2022-08-12 13:38:21 +0300

    Add files via upload

---
+++ author.json.py
@@ -0,0 +1,19 @@
+import json
+
+{
+    "name": "İbrahim",
+    "surname": "Dellalbaşı",
+    "hobbies": ["running", "sky diving", "singing"],
+    "age": 21,
+    "author: İbrahim Dellalbaşı"
+    "education": [
+        {
+            "University": "İstanbul Üniversitesi",
+                        "Undergraduate": "Economics",
+
+        },
+        {
+
+        }
+    ]
+}
